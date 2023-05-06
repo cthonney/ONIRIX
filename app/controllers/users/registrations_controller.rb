b639b6ad2ad39df2f291e5ff3311ffbf214d538e
Thonney Cedric <c.thonney@gmail.com>
2023-05-06 21:28:57 +0400

    add verif registration

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -10,9 +10,19 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+# app/controllers/sessions_controller.rb
+  def create
+    success = verify_recaptcha(action: 'login', minimum_score: 0.5, secret_key: ENV['RECAPTCHA_SECRET_KEY_V3'])
+    checkbox_success = verify_recaptcha unless success
+    if success || checkbox_success
+      super
+    else
+      if !success
+        @show_checkbox_recaptcha = true
+      end
+      render 'new'
+    end
+  end
 
   # GET /resource/edit
   # def edit
