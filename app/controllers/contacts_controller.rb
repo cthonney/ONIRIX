bebb0d6ab2ed4e5def3d60070a0d2057ae69bf35
Thonney Cedric <c.thonney@gmail.com>
2023-05-06 23:19:33 +0400

    create contact form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,7 +1,22 @@
 class ContactsController < ApplicationController
+  skip_before_action :authenticate_user!
+
   def new
+    @contact = Contact.new
   end
 
   def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      redirect_to new_contact_path, notice: "Votre message a été envoyé avec succès."
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name, :email, :message)
   end
 end
