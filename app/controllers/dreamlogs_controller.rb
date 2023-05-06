4b7fa6781f43f88ce3ade10bb00d6923ac940f5b
Christian Bongard <christian.bongard90@gmail.com>
2023-05-06 23:57:25 +0400

    added the new page

--- app/controllers/dreamlogs_controller.rb
+++ app/controllers/dreamlogs_controller.rb
@@ -11,4 +11,20 @@ class DreamlogsController < ApplicationController
   def new
     @dreamlog = Dreamlog.new
   end
+
+  def create
+    @dreamlog = Dreamlog.new
+    @dreamlog.user = current_user
+    if @dreamlog.save
+      redirect_to dreamlog_path(@dreamlog)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def dreamlog_params
+    params.require(:dreamlog).permit
+  end
 end
