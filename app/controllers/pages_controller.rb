24f96f1dcfb370beb61382a26800e3caf0bd7bae
Christian Bongard <christian.bongard90@gmail.com>
2023-05-06 22:52:14 +0400

    added the blog page and about page content

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,5 +11,49 @@ class PagesController < ApplicationController
   end
 
   def blog
+    @blogs = [
+      {
+        title: "Dreams and the Subconscious",
+        date: "2020-10-01",
+        author: "J. Doe",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl quis aliquam ultricies, nunc nisl ultricies nunc, vitae aliquam nisl nunc vitae nisl. Sed euismod, nisl quis aliquam ultricies, nunc nisl ultricies nunc, vitae aliquam nisl nunc vitae nisl. Sed euismod, nisl quis aliquam ultricies, nunc nisl ultricies nunc, vitae aliquam nisl nunc vitae nisl. Sed euismod, nisl quis aliquam ultricies, nunc nisl ultricies nunc, vitae aliquam nisl nunc vitae nisl."
+      },
+      {
+        title: "Dreams and the Subconscious",
+        date: "2023-10-01",
+        author: "J. Doe",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl quis aliquam ultricies, nunc nisl ultricies nunc, vitae aliquam nisl nunc vitae nisl. Sed euismod, nisl quis aliquam ultricies, nunc nisl ultricies nunc, vitae aliquam nisl nunc vitae nisl. Sed euismod, nisl quis aliquam ultricies, nunc nisl ultricies nunc, vitae aliquam nisl nunc vitae nisl. Sed euismod, nisl quis aliquam ultricies, nunc nisl ultricies nunc, vitae aliquam nisl nunc vitae nisl."
+      },
+      {
+        title: "Dreams and the Subconscious",
+        date: "2022-10-01",
+        author: "J. Doe",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl quis aliquam ultricies, nunc nisl ultricies nunc, vitae aliquam nisl nunc vitae nisl. Sed euismod, nisl quis aliquam ultricies, nunc nisl ultricies nunc, vitae aliquam nisl nunc vitae nisl. Sed euismod, nisl quis aliquam ultricies, nunc nisl ultricies nunc, vitae aliquam nisl nunc vitae nisl. Sed euismod, nisl quis aliquam ultricies, nunc nisl ultricies nunc, vitae aliquam nisl nunc vitae nisl."
+      },
+      {
+        title: "Dreams and the Subconscious",
+        date: "2021-10-01",
+        author: "J. Doe",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl quis aliquam ultricies, nunc nisl ultricies nunc, vitae aliquam nisl nunc vitae nisl. Sed euismod, nisl quis aliquam ultricies, nunc nisl ultricies nunc, vitae aliquam nisl nunc vitae nisl. Sed euismod, nisl quis aliquam ultricies, nunc nisl ultricies nunc, vitae aliquam nisl nunc vitae nisl. Sed euismod, nisl quis aliquam ultricies, nunc nisl ultricies nunc, vitae aliquam nisl nunc vitae nisl."
+      },
+      {
+        title: "Dreams and the Subconscious",
+        date: "2020-10-01",
+        author: "J. Doe",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl quis aliquam ultricies, nunc nisl ultricies nunc, vitae aliquam nisl nunc vitae nisl. Sed euismod, nisl quis aliquam ultricies, nunc nisl ultricies nunc, vitae aliquam nisl nunc vitae nisl. Sed euismod, nisl quis aliquam ultricies, nunc nisl ultricies nunc, vitae aliquam nisl nunc vitae nisl. Sed euismod, nisl quis aliquam ultricies, nunc nisl ultricies nunc, vitae aliquam nisl nunc vitae nisl."
+      },
+      {
+        title: "Dreams and the Subconscious",
+        date: "2020-10-01",
+        author: "J. Doe",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl quis aliquam ultricies, nunc nisl ultricies nunc, vitae aliquam nisl nunc vitae nisl. Sed euismod, nisl quis aliquam ultricies, nunc nisl ultricies nunc, vitae aliquam nisl nunc vitae nisl. Sed euismod, nisl quis aliquam ultricies, nunc nisl ultricies nunc, vitae aliquam nisl nunc vitae nisl. Sed euismod, nisl quis aliquam ultricies, nunc nisl ultricies nunc, vitae aliquam nisl nunc vitae nisl."
+      },
+      {
+        title: "Dreams and the Subconscious",
+        date: "2020-10-01",
+        author: "J. Smith",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl quis aliquam ultricies, nunc nisl ultricies nunc, vitae aliquam nisl nunc vitae nisl. Sed euismod, nisl quis aliquam ultricies, nunc nisl ultricies nunc, vitae aliquam nisl nunc vitae nisl. Sed euismod, nisl quis aliquam ultricies, nunc nisl ultricies nunc, vitae aliquam nisl nunc vitae nisl. Sed euismod, nisl quis aliquam ultricies, nunc nisl ultricies nunc, vitae aliquam nisl nunc vitae nisl."
+      }
+    ]
   end
 end
